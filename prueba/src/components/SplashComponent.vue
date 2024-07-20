0d314bec5ff3396aa6e3b20d0d9491b022634418
jrubioh1 <rubiocuri@gmail.com>
2024-07-21 00:07:02 +0200

    .

--- prueba/src/components/SplashComponent.vue
+++ prueba/src/components/SplashComponent.vue
@@ -15,7 +15,7 @@
 
                     <v-expand-transition>
                         <v-sheet v-if="isVisible" class="pb-2 mb-2" max-width="100" color="transparent">
-                            <v-img src="@/assets/logo.png" class="" alt="Logo" max-height="600" max-width="100" /
+                            <v-img src="@/assets/logo.png" class="" alt="Logo" max-height="600" max-width="100" />
                         </v-sheet>
                     </v-expand-transition>
 
