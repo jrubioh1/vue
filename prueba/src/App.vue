6b7a48f85359eb67acea94f26a98c6e7dbf588b9
jrubioh1 <rubiocuri@gmail.com>
2024-07-20 13:46:24 +0200

    pantalla de carga

--- prueba/src/App.vue
+++ prueba/src/App.vue
@@ -1,20 +1,41 @@
 <template>
-  <v-app>
-   <HeaderLayout/>
-
-       <v-parallax class="pa-8">
-        
-        <v-main>
-          <router-view />
+  <div id="app">
+    <transition name="fade">
+      <SplashComponent v-show="showSplash" />
+    </transition>
+    <transition name="fade">
+      <div v-show="!showSplash">
+        <v-app>
+          <HeaderLayout />
+          <v-main class="pa-8">
+            <router-view />
           </v-main>
-
-      </v-parallax>
-      <FooterLayout/>
-  </v-app>
+          <FooterLayout />
+        </v-app>
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import HeaderLayout from '@/layouts/HeaderLayout.vue'
-import FooterLayout from '@/layouts/FooterLayout.vue' 
-  //
+import FooterLayout from '@/layouts/FooterLayout.vue'
+import SplashComponent from '@/components/SplashComponent.vue'
+
+let showSplash = ref(true)
+
+onMounted(() => {
+  setTimeout(() => {
+    showSplash.value = false
+  }, 3000)
+})
 </script>
+<style>
+  .fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0;
+}
+</style>
---
+++ prueba/src/components/SplashComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-app>
+        <div class="container">
+            <img src="@/assets/logo.png" class="logo" alt="Logo" />
+            <div class="progress-container">
+                <v-progress-circular :size="60" :width="3" indeterminate color="primary"></v-progress-circular>
+            </div>
+        </div>
+    </v-app>
+</template>
+  
+<script setup>
+
+
+</script>
+
+<style>
+.container {
+    display: flex;
+    /* Activa Flexbox */
+    flex-direction: column;
+    /* Organiza los elementos en columna */
+    justify-content: center;
+    /* Centra los elementos verticalmente */
+    align-items: center;
+    /* Centra los elementos horizontalmente */
+    height: 100vh;
+    /* Ocupa toda la altura de la ventana */
+    width: 100vw;
+    /* Ocupa toda la anchura de la ventana (opcional) */
+}
+
+
+.logo {
+    max-width: 200px;
+    /* Limita el ancho máximo de la imagen */
+    margin-bottom: 16px;
+    /* Espacio debajo de la imagen */
+}
+
+
+.progress-container {
+    margin-top: 50px;
+
+}</style> 
