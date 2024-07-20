9c1fe06aae57e98a1c443fa30aeb5f64189a851f
jrubioh1 <rubiocuri@gmail.com>
2024-07-20 17:26:10 +0200

    modificando splash y header para aplicar v-flex

--- prueba/src/components/SplashComponent.vue
+++ prueba/src/components/SplashComponent.vue
@@ -4,9 +4,11 @@
         <v-container class=" mb-6 fill-height">
             <v-row align="center" max-height="150" no-gutters>
                 <v-col align="center">
-                    <v-sheet class="pa-2 ma-2" max-width="100">
-                        <v-img src="@/assets/logo.png" class="" alt="Logo" max-height="600" max-width="100" />
-                    </v-sheet>
+                    <v-expand-transition>
+                        <v-sheet v-if="isVisile" class="pa-2 ma-2" max-width="100">
+                            <v-img src="@/assets/logo.png" class="" alt="Logo" max-height="600" max-width="100" />
+                        </v-sheet>
+                    </v-expand-transition>
                     <v-sheet class="mt-15" max-width="200">
                         <v-progress-linear rounded color="blue-lighten-3" indeterminate></v-progress-linear>
                     </v-sheet>
@@ -20,6 +22,16 @@
   
 <script setup>
 
+import { ref , onMounted} from 'vue'
+
+const isVisible = ref(true)
+
+onMounted(()=>{
+    setTimeout(() => {
+        isVisible.value = !isVisible.value
+        
+    }, 500)
+})
 
 </script>
 
